fdee678c5bbe3bb07d5ac98404ed8023a846f91d
Nouman Javed <noumanmalik960@gmail.com>
2020-09-08 14:21:57 +0500

    all working till global data

---
+++ src/components/GlobalData.js
@@ -0,0 +1,124 @@
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        '& > *': {
+            margin: theme.spacing(1),
+            width: '100%',
+            height: theme.spacing(16),
+        },
+    },
+}));
+
+
+
+// this is supposed to be the styling for the Typography component
+// const useStylesTypography = makeStyles({
+//     root: {
+//         width: '100%',
+//         maxWidth: 500,
+//     }
+// })
+
+export default function GlobalData() {
+    const classes = useStyles();
+
+    const [globalData, setGlobalData] = useState();
+    const [isLoading, setLoading] = useState(false);
+    const loading = "Loading...";
+
+
+    useEffect(() => {
+        async function fetchGlobalData() {
+            setLoading(true);
+            const apiResponse = await fetch('https://api.thevirustracker.com/free-api?global=stats');
+            const dataFromAPI = await apiResponse.json();
+            setLoading(false)
+            setGlobalData(dataFromAPI)
+        }
+        fetchGlobalData();
+    }, [])
+  
+    if(isLoading) {
+        return (
+            <div className={classes.root}>
+                <Paper elevation={3} style={{ paddingTop: '20px' }}>
+                    <Typography variant="h5" gutterBottom>
+                        Global Cases
+                </Typography>
+                    <Typography variant="subtitle1" gutterBottom>
+                        {loading}
+                </Typography>
+                </Paper>
+                <Paper elevation={3} style={{ color: "orange", paddingTop: '20px' }}>
+                    <Typography variant="h5" gutterBottom>
+                        Active
+                </Typography>
+                    <Typography variant="subtitle1" gutterBottom>
+                    {loading}
+                </Typography>
+                </Paper>
+                <Paper elevation={3} style={{ color: "green", paddingTop: '20px' }}>
+                    <Typography variant="h5" gutterBottom>
+                        Recovered
+                </Typography>
+                    <Typography variant="subtitle1" gutterBottom>
+                    {loading}
+                </Typography>
+                </Paper>
+                <Paper elevation={3} style={{ color: "red", paddingTop: '20px' }}>
+                    <Typography variant="h5" gutterBottom>
+                        Casualities
+                </Typography>
+                    <Typography variant="subtitle1" gutterBottom>
+                    {loading}
+                </Typography>
+                </Paper>
+            </div>
+        );
+    }
+
+
+
+    return (
+        <div className={classes.root}>
+            <Paper elevation={3} style={{ paddingTop: '20px' }}>
+                <Typography variant="h5" gutterBottom>
+                    Global Cases
+            </Typography>
+                <Typography variant="subtitle1" gutterBottom>
+                    {globalData && globalData.results[0].total_cases.toLocaleString()}
+            </Typography>
+            </Paper>
+            <Paper elevation={3} style={{ color: "orange", paddingTop: '20px' }}>
+                <Typography variant="h5" gutterBottom>
+                    Active
+            </Typography>
+                <Typography variant="subtitle1" gutterBottom>
+                {globalData && globalData.results[0].total_unresolved.toLocaleString()}
+            </Typography>
+            </Paper>
+            <Paper elevation={3} style={{ color: "green", paddingTop: '20px' }}>
+                <Typography variant="h5" gutterBottom>
+                    Recovered
+            </Typography>
+                <Typography variant="subtitle1" gutterBottom>
+                {globalData && globalData.results[0].total_recovered.toLocaleString()}
+            </Typography>
+            </Paper>
+            <Paper elevation={3} style={{ color: "red", paddingTop: '20px' }}>
+                <Typography variant="h5" gutterBottom>
+                    Casualities
+            </Typography>
+                <Typography variant="subtitle1" gutterBottom>
+                {globalData && globalData.results[0].total_deaths.toLocaleString()}
+            </Typography>
+            </Paper>
+        </div>
+    );
+}
---
+++ src/components/MainGrid.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import GlobalData from './GlobalData';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+export default function MainGrid() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={2}>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>
+              <GlobalData />
+          </Paper>
+        </Grid>
+        <Grid item xs={8}>
+          <Paper className={classes.paper}>
+              Country Data
+          </Paper>
+        </Grid>
+      </Grid>
+     
+    </div>
+  );
+}
