734fd2013d1d3e5b1e3be7f3d33e05897397cc2c
Nouman Javed <noumanmalik960@gmail.com>
2020-09-15 09:42:03 +0500

    added github link

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import './Header.css'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -26,6 +27,9 @@ export default function Header() {
           <Typography variant="h5" className={classes.title}>
             Covid-19 Tracker
           </Typography>
+          <address >
+            <a id='github' href="https://github.com/noumanmalik960/covid19-tracker">GitHub</a>
+          </address>
         </Toolbar>
       </AppBar>
     </div>
